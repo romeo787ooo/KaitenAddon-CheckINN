f5cfc29ab135973103bda6209abfb813420c9887
romeo787ooo <randriyanov.ooo@mail.ru>
2024-11-28 11:38:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.static(path.join(__dirname, 'public/views')));
 app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+
+// Добавляем обработку корневого index.html
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 const port = process.env.PORT || 1111;
 
